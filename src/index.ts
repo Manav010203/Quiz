99feb5759c0d93f43a400658f1aba0fe01b49e5b
Manav010203 <manav01022003@gmail.com>
2025-01-02 23:59:41 +0530

    f

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import http from 'http';
+import { Server } from 'socket.io';
+
+const server= http.createServer();
+
+const io = new Server(server);
+
+io.on('connection',(client)=>{
+    client.on('event',data=>{
+        const type = data.type;
+     });
+    client.on('disconnect',()=>{});
+})
+
+io.listen(3000)
